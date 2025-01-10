7987281c8b979fc954b5f117490a8f4d0f07591e
yuchen <44870300+yuchenz27@users.noreply.github.com>
2025-01-10 21:32:42 +0800

    Delete VulkanTesting directory

--- VulkanTesting/VulkanTesting/V1/compile.sh
+++
@@ -1,2 +0,0 @@
-/Users/yuchen/Applications/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-/Users/yuchen/Applications/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
