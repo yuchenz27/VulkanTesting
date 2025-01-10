b856501026800cd6f52ab0e6249534e469f7146d
yuchen <yuchenz27@outlook.com>
2025-01-10 21:31:40 +0800

    initial commit

---
+++ VulkanTesting/VulkanTesting/V1/compile.sh
@@ -0,0 +1,2 @@
+/Users/yuchen/Applications/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/Users/yuchen/Applications/VulkanSDK/1.3.296.0/macOS/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
---
+++ VulkanTesting/VulkanTesting/V1/first_app.cc
@@ -0,0 +1,11 @@
+#include "first_app.h"
+
+namespace lve {
+
+void FirstApp::run() {
+    while(!lveWindow.shouldClose()) {
+        glfwPollEvents();
+    }
+}
+
+}
